5d641de72dd642ec7c0fe9be8d6c49f44374bba7
Manas <iamscientistmanas@gmail.com>
2023-08-08 18:45:35 +0530

    Insertion sort by while

--- Sortings/InsertionSort.cpp
+++ Sortings/InsertionSort.cpp
@@ -20,3 +20,15 @@ int main() {
         arr[j+1] = temp;
     }
 }
+
+// While loop approach
+
+// for (int i = 1 ; i<n ; i++) {
+//         int temp = arr[i] ;
+//         int j = i - 1;
+//         while (arr[j]>temp)  {
+//             arr[j+1] = arr[j];
+//             j--;
+//         }
+//         arr[j+1] = temp;
+//     }
