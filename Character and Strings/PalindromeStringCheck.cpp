5b9a6d38117499fac1c1f917196c0f81d13a10f8
Manas <iamscientistmanas@gmail.com>
2023-08-16 16:18:58 +0530

    Plaindrome with removig nonalphanumeric character

--- Character and Strings/PalindromeStringCheck.cpp
+++ Character and Strings/PalindromeStringCheck.cpp
@@ -3,14 +3,72 @@
 
 using namespace std;
 
-bool CheckPalindrome(string &s) {
-    int n = s.length();
-    string reverseone;
-    
-
+bool IsCharcater(char ch)
+{
+    if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+        return 1;
+    return 0;
 }
 
-int main() {
-    string s = "";
+void ReverseTheString(string &s)
+{ // Working
+    int n = s.length();
+    string reverseone = s;
+    int st = 0;
+    int e = n - 1;
+    while (st <= e)
+    {
+        swap(reverseone[st], reverseone[e]);
+        st++;
+        e--;
+    }
+    s = reverseone;
+}
+
+bool CheckPalindrome(string &s)
+{
+    int st = 0;
+    int e = s.length() - 1;
+    while (st <= e)
+    {
+        if (s[st] != s[e])
+            return false;
+        st++;
+        e--;
+    }
+    return true;
+}
+
+bool CheckPalindromeWithRemovedSpacesAndNonAlphaNumericCharacters(string &s)
+{
+    int st = 0;
+    int e = s.length() - 1;
+    while (st <= e)
+    {
+        if (!IsCharcater(s[st]))
+        {
+            st++;
+            continue;
+        }
+        if (!IsCharcater(s[e]))
+        {
+            e--;
+            continue;
+        }
+        if (tolower(s[st]) != tolower(s[e]))
+            return false;
+        st++;
+        e--;
+    }
+    return true;
+}
+
+int main()
+{
+    // string s = "AABCDEFFEDCBAA$#%^$";
+    string s = "A man, a plan, a canal: Panama";
+    // ReverseTheString(s);
+    // cout<<"Reversed String: "<<s<<endl;
     cout<<"Is Palindrome: "<<CheckPalindrome(s)<<endl;
+    cout << "Is Plaindrome with faltu ke character hatake: " << CheckPalindromeWithRemovedSpacesAndNonAlphaNumericCharacters(s) << endl;
 }
