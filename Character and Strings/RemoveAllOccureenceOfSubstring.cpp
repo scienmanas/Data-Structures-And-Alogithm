e414f0573accecf7092876e482726d86c795e774
Manas <iamscientistmanas@gmail.com>
2023-08-18 17:32:08 +0530

    Problem done

--- Character and Strings/RemoveAllOccureenceOfSubstring.cpp
+++ Character and Strings/RemoveAllOccureenceOfSubstring.cpp
@@ -4,12 +4,15 @@
 
 using namespace std;
 
-string removeOccurrences(string s, string part) {
-
+string RemoveOccurrences(string &s, string part) {
+    while (s.length()!=0 && s.find(part)<s.length()) {
+        s.erase(s.find(part),part.length());
+    }
+    return s;
 }
 
 int main() {
     string s = "daabcbaabcbc";
     string part = "dab";
-    t
+    RemoveOccurrences(s, part);
 }
