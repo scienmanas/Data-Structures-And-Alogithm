71685d518948e61c57e3c0e44f12807498db8915
Manas <iamscientistmanas@gmail.com>
2023-08-26 18:38:23 +0530

    math

--- Maths/ModularMultiplicativeinverse.cpp
+++ Maths/ModularMultiplicativeinverse.cpp
@@ -6,7 +6,12 @@ using namespace std;
 // The modular multiplicative inverse of a function a is (a*x)%m = 1
 // Now, a and m should be coprime, i.e., HCF/GCD = 1 or they have no common divisor except 1
 
-int brutefoce(int a, int m) {
+
+int optimized(int a, int m) {
+    
+}
+
+int brutefoce(int a, int m) {         
     for(int i = 1; i<=m-1 ; i++) {
         if((a*i)%m == 1) {
             return i;
@@ -14,6 +19,9 @@ int brutefoce(int a, int m) {
     }
     return -1;
 }
+
+// Time Complexity - O(m)
+//Space Complexity - O(1)
 
 int main() {
     int a = 10;
