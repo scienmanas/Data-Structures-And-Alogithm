f531435a42c4f5d132b7eaf93a649e9a9f232f13
Manas <iamscientistmanas@gmail.com>
2023-09-12 19:46:34 +0530

    Add leetcode problems

--- LeetCode Problems/Medium/4Sum.cpp
+++ LeetCode Problems/Medium/4Sum.cpp
@@ -11,5 +11,12 @@ vector<vector<int>> fourSum(vector<int> &nums, int target)
 
 int main()
 {
-    
+    vector<int> nums;
+    int target;
+
+    nums = {1,0,-1,0,-2,2};
+    target = 0;
+
+    nums = {2,2,2,2,2};
+    target = 8;
 }
