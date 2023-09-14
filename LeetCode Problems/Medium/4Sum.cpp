a69ad7e590130f0e4bb1a48d7160ccba983ed814
Manas <iamscientistmanas@gmail.com>
2023-09-15 03:30:36 +0530

    4sum done

--- LeetCode Problems/Medium/4Sum.cpp
+++ LeetCode Problems/Medium/4Sum.cpp
@@ -7,40 +7,38 @@ using namespace std;
 vector<vector<int>> fourSum(vector<int> &nums, int target)
 {
     sort(nums.begin(), nums.end());
-    int i = 0;
-    // vector<int> dummy(4);
     vector<vector<int>> final;
-    if(nums.size() < 4) {
+    if (nums.size() < 4)
+    {
         return final;
     }
+
+    int i = 0;
     while (i < nums.size() - 3)
     {
-        // cout << "Lawde -1 " << endl;
         int j = i + 1;
         while (j < nums.size() - 2)
         {
-            // cout << "Lawde 2" << endl;
             int k = j + 1;
             int l = nums.size() - 1;
             while (k < l)
             {
-                while (nums[l] == nums[l - 1] && l > k)
-                {
-                    l--;
-                }
-                // cout << "Lawde 3" << endl;
-                while (nums[k] == nums[k + 1] && k < l)
-                {
-                    k++;
-                }
-
-                if ((nums[i] + nums[j] + nums[k] + nums[l]) == target)
+                long long sum = static_cast<long long>(nums[i]) + nums[j] + nums[k] + nums[l];
+                if (sum == target)
                 {
                     final.push_back({nums[i], nums[j], nums[k], nums[l]});
                     k++;
                     l--;
+                    while (nums[l] == nums[l + 1] && l > k)
+                    {
+                        l--;
+                    }
+                    while (nums[k] == nums[k - 1] && k < l)
+                    {
+                        k++;
+                    }
                 }
-                else if ((nums[i] + nums[j] + nums[k] + nums[l]) > target)
+                else if (sum > target)
                 {
                     l--;
                 }
@@ -49,15 +47,29 @@ vector<vector<int>> fourSum(vector<int> &nums, int target)
                     k++;
                 }
             }
-            while (nums[j] == nums[j + 1] && j + 1 < nums.size() - 2)
+            for (; j + 1 < nums.size() - 2; j++)
             {
-                j++;
+                if (nums[j] == nums[j + 1])
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
             }
             j++;
         }
-        while (nums[i] == nums[i + 1] && i + 1 < nums.size() - 3)
+        for (; i + 1 < nums.size() - 3; i++)
         {
-            i++;
+            if (nums[i] == nums[i + 1])
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
         }
         i++;
     }
@@ -69,9 +81,17 @@ int main()
     vector<int> nums;
     int target;
 
-    // nums = {1, 0, -1, 0, -2, 2};
-    // target = 0;
-
+    nums = {1, 0, -1, 0, -2, 2};
+    target = 0;
+    for (auto i : fourSum(nums, target))
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     nums = {2, 2, 2, 2, 2};
     target = 8;
     for (auto i : fourSum(nums, target))
